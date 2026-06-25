3ec94644cb71840695f6893f5b044019d3087518
Giovanny Hernandez Arbelaez <giovanni104@hotmail.com>
2026-06-25 17:43:15 -0500

    add projects angular commit

---
+++ angular/angular-app/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CounterComponent, CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title: string = 'Hola mundo angular desde componente!';
+  
+  subTitle = 'Contador con estado de session';
+  
+  users: string[] = ['Pepe', 'Maria', 'Juan', 'Andres'];
+  
+  visible: boolean = false;
+  
+  counter: number = 0;
+  ngOnInit(): void {
+    this.counter = parseInt(sessionStorage.getItem('counter')!) || 0;
+  }
+
+  setVisible(): void {
+    this.visible = this.visible ? false : true;
+    console.log('hemos hecho clic en setVisible');
+  }
+
+  setCounter(counter: number): void{
+    this.counter = counter
+    
+  }
+}
